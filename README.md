ddda4acba1c114dc9121eacd443fd119d3372578
backmeupplz <backmeupplz@gmail.com>
2021-11-07 18:46:15 -0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Frontend code template in React
+# Is Bot Down website code
 
-This template is to be used for frontend applications.
+This is the website code for [isbotdown.com](https://isbotdown.com).
 
 ## Local launch
 
@@ -11,3 +11,7 @@ This template is to be used for frontend applications.
 
 - `yarn start` — runs the app in the development mode
 - `yarn build` — builds the app for production to the `docs` folder
+
+## CI/CD
+
+`docs` folder of the `main` page gets deployed to [isbotdown.com](https://isbotdown.com).
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,8 @@ module.exports = {
       colors: {
         black: {
           background: '#040404',
-          'background-purpose': '#12141D',
-          'perk-gradient-light': 'rgba(4, 6, 14, 0.9)',
-          'perk-gradient-dark': '#04060E',
-        },
-        gold: {
-          dark: '#FA6641',
-          light: '#EAE100',
         },
       },
-      borderRadius: {
-        avatar: '6rem',
-      },
-      width: {
-        chart: '108px',
-        time: '95px',
-        glass: '107px',
-      },
-    },
-    container: {
-      center: true,
-      padding: '2rem',
     },
   },
 }
