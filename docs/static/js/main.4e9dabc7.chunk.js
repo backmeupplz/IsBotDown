654a564b4eae8c2d08b3e4bd67f9cc141aa6c49e
backmeupplz <backmeupplz@gmail.com>
2021-11-08 17:29:05 -0800

    update

--- docs/static/js/main.4e9dabc7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfrontend-starter"]=this["webpackJsonpfrontend-starter"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);e(5);var r=e(3).a,c=e(0),s=r("container","mx-auto","pb-10","max-w-4xl","py-4"),a=function(t){var n=t.children;return Object(c.jsx)("div",{className:s,children:n})},o=function(){return Object(c.jsx)(a,{})},i=e(1),d=e.n(i),u=e(4);e.n(u).a.render(Object(c.jsx)(d.a.StrictMode,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))},5:function(t,n,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.4e9dabc7.chunk.js.map
