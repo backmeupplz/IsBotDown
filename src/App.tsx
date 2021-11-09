786f32357bf670d27611e8dcb3d0b13dfb535d94
backmeupplz <backmeupplz@gmail.com>
2021-11-08 18:22:02 -0800

    move to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import Bot from 'pages/Bot'
 import Main from 'pages/Main'
 import NotFound from 'pages/NotFound'
